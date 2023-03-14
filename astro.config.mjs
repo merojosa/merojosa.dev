17cd7bd933938fca3586e8d0d824a73486bb65f2
Jose Andrés Mejías Rojas <jose.andresmr@hotmail.com>
2023-03-14 00:20:36 -0600

    Installing MDX

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,13 @@
 import { vanillaExtractPlugin } from "@vanilla-extract/vite-plugin";
 import { defineConfig } from "astro/config";
-
 import react from "@astrojs/react";
+
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
-    plugins: [vanillaExtractPlugin()],
+    plugins: [vanillaExtractPlugin()]
   },
-  integrations: [react()],
+  integrations: [react(), mdx()]
 });
