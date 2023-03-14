212a69e17b60c1fae8b21a3fa5d825602b5a61e5
Jose Andrés Mejías Rojas <jose.andresmr@hotmail.com>
2023-03-14 01:04:26 -0600

    Blog first page

---
+++ src/content/config.ts
@@ -0,0 +1,20 @@
+import { z, defineCollection } from "astro:content";
+
+const blogCollection = defineCollection({
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    pubDate: z
+      .string()
+      .or(z.date())
+      .transform((val) => new Date(val)),
+    updatedDate: z
+      .string()
+      .optional()
+      .transform((str) => (str ? new Date(str) : undefined)),
+  }),
+});
+
+export const collections = {
+  blog: blogCollection,
+};
