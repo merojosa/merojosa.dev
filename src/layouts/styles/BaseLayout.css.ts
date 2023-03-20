8fe1d5a0cc43d495e12434529d27ba0e89a3838c
Jose Andrés Mejías Rojas <jose.andresmr@hotmail.com>
2023-03-20 01:56:00 -0600

    Fix layout

--- src/layouts/styles/BaseLayout.css.ts
+++
@@ -1,8 +0,0 @@
-import { style } from "@vanilla-extract/css";
-
-const blogLayoutSpace = "31rem";
-
-export const blogMainStyle = style({
-  marginLeft: blogLayoutSpace,
-  marginRight: blogLayoutSpace,
-});
