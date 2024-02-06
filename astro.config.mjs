505b35fef26a01adb9b8726dd867b3b33c8987c7
Jose <jose.andresmr@hotmail.com>
2024-02-06 15:07:53 -0600

    Fix config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,7 @@
-import { vanillaExtractPlugin } from "@vanilla-extract/vite-plugin";
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  vite: {
-    plugins: [vanillaExtractPlugin()]
-  },
   integrations: [react()]
 });
