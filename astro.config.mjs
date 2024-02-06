07c2bb0a14c6defd80620b6ab4167e5009eca35b
Jose <jose.andresmr@hotmail.com>
2024-02-06 14:52:52 -0600

    Remove blog

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,10 @@ import { vanillaExtractPlugin } from "@vanilla-extract/vite-plugin";
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 
-import mdx from "@astrojs/mdx";
-
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [vanillaExtractPlugin()]
   },
-  integrations: [react(), mdx()]
+  integrations: [react()]
 });
--- src/shared/utils/dates.util.ts
+++
@@ -1,7 +0,0 @@
-export const getDateForTimeTag = (date: Date) => ({
-    dateDisplay: new Intl.DateTimeFormat("local", {
-        dateStyle: "long",
-    }).format(date),
-    datetime: `${date.getFullYear()}-${date.getMonth() + 1
-        }-${date.getDate()}`
-} as const)
