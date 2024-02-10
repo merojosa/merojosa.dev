f8f79abb8402c49efa2cf6e5ed63fdb2ea9a3ec9
Jose <jose.andresmr@hotmail.com>
2024-02-10 01:05:32 -0600

    Deploy to AWS

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from "astro/config";
-import react from "@astrojs/react";
+import aws from "astro-sst";
 
-// https://astro.build/config
 export default defineConfig({
-  integrations: [react()]
+  output: "server",
+  adapter: aws(),
 });
