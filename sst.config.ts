3031b0480d02a3e659a2659814cca6ec174af379
Jose <jose.andresmr@hotmail.com>
2024-03-25 09:44:23 -0600

    Fix image optimization by adding sharp layer

--- sst.config.ts
+++ sst.config.ts
@@ -1,5 +1,6 @@
 import type { SSTConfig } from "sst";
 import { AstroSite } from "sst/constructs";
+import { aws_lambda } from 'aws-cdk-lib'
 
 export default {
   config(_input) {
@@ -14,6 +15,28 @@ export default {
         customDomain: {
           domainName: "jose-andres.dev",
           domainAlias: "www.jose-andres.dev"
+        },
+        memorySize: '1024 MB',
+        runtime: "nodejs20.x",
+        nodejs: {
+          esbuild: {
+            external: ['sharp'] // Ensures Sharp is excluded from the code bundle.
+          }
+        },
+        cdk: {
+          server: {
+            layers: [
+              new aws_lambda.LayerVersion(this, 'sharp', {
+                /**
+                 * This is a prebuilt layer for sharp.
+                 * Source: https://github.com/pH200/sharp-layer
+                 */
+                code: aws_lambda.Code.fromAsset('./layers/sharp.zip'),
+                compatibleRuntimes: [aws_lambda.Runtime.NODEJS_20_X],
+                compatibleArchitectures: [aws_lambda.Architecture.ARM_64]
+              })
+            ]
+          },
         },
       });
       stack.addOutputs({
